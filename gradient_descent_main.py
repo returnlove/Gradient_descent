ef2fa055fe8dfa7d4555206c58289a8caf7e3112
Gaddam <sainath.gaddam@accenture.com>
2018-06-11 01:01:32 +0530

    compute cost for given points with random weights

--- gradient_descent_main.py
+++ gradient_descent_main.py
@@ -1,2 +1,36 @@
 # -*- coding: utf-8 -*-
 
+# y = mx + b
+
+# ideally m= (y2-y1)/(x2-x1), once we know m, we can derive value of b
+# but let's do gradient descent to find best parameters
+
+from numpy import *
+
+
+
+def compute_cost_given_points(b,m,points):
+    total_error = 0
+    for i in range(0,len(points)):
+        x = points[i, 0] # for each row, first column is x
+        y = points[i, 0] # and second column is y
+        total_error += (y - (m*x + b)) **2 # formula: https://spin.atomicobject.com/wp-content/uploads/linear_regression_error1.png
+        #print(total_error)
+    return total_error / float(len(points))
+    
+def step_gradient(b_current, m_current, points, learing_rate):
+    b_gradient = 0
+    m_gradient= 0
+    
+    for i in range(0, len(points)):
+        x = points[i,0]
+        y = points[i,1]
+    
+
+                
+points =  genfromtxt("data.csv", delimiter = ",")
+#print(points)     
+initial_b = 0  # initial y-intercept guess
+initial_m = 0.5 # initial slope guess
+print(compute_cost_given_points(initial_b, initial_m, points))
+    
