2dee5dc44d59ccce4ab363195650c2a4ce2dc833
Gaddam <sainath.gaddam@accenture.com>
2018-06-12 23:42:18 +0530

    added gradient desent logic

--- gradient_descent_main.py
+++ gradient_descent_main.py
@@ -18,13 +18,19 @@ def compute_cost_given_points(b,m,points):
         #print(total_error)
     return total_error / float(len(points))
     
-def step_gradient(b_current, m_current, points, learing_rate):
+def step_gradient(b_current, m_current, points, learningRate):
     b_gradient = 0
     m_gradient= 0
-    
     for i in range(0, len(points)):
         x = points[i,0]
         y = points[i,1]
+        b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
+        m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
+    new_b = b_current - (learningRate * b_gradient)
+    new_m = m_current - (learningRate * m_current)
+    return [new_b, new_m]
+
+
     
 
                 
